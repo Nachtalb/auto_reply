48fa906827751f9075b0a43ac8429c8cc48f1d96
Nachtalb <info@nachtalb.io>
2021-09-01 00:23:45 +0200

    Move generic plugin code to a base class to not disturb the main feature

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from threading import Event, Thread
 from time import sleep, time
 from urllib import request
 
-from pynicotine.pluginsystem import BasePlugin
+from pynicotine.pluginsystem import BasePlugin as NBasePlugin
 
 BASE_PATH = Path(__file__).parent
 config_file = BASE_PATH / 'PLUGININFO'
@@ -13,53 +13,6 @@ CONFIG = dict([(key, ast.literal_eval(value) if value.startswith('[') else value
                for key, value in map(lambda i: i.split('=', 1), filter(None, config_file.read_text().split('\n')))])
 
 __version__ = CONFIG.get('Version', '0.0.1')
-__version__ = __version__.replace('dev', '')
-
-
-class Plugin(BasePlugin):
-
-    settings = {
-        'check_update': True,
-    }
-    metasettings = {
-        'check_update': {
-            'description': '''Check for Updates
-Check for updates on start and periodically''',
-            'type': 'bool',
-        },
-    }
-    __name__ = CONFIG.get('Name')
-    update_version = None
-
-    def init(self):
-        self.auto_update = PeriodicJob(name='AutoUpdate',
-                                       delay=3600,
-                                       update=self.check_update)
-        self.auto_update.start()
-        self.log(f'Running version {__version__}')
-
-    def check_update(self):
-        try:
-            repo = CONFIG.get('Repository')
-            if 'dev' in __version__ or not repo or not self.settings['check_update']:
-                self.update_version = None
-                return
-
-            self.log('Checking for updates')
-            with request.urlopen(f'https://api.github.com/repos/{repo}/tags') as response:
-                latest_info = next(iter(json.load(response)), {})
-                latest_version = latest_info.get('name', '')
-                if latest_version.replace('v', '') != __version__:
-                    self.update_version = latest_version
-                    msg = f'# A new version of "{self.__name__}" is available: {latest_version} https://github.com/{repo}/releases/tag/{latest_version}'  # noqa
-                    self.log('\n{border}\n{msg}\n{border}'.format(msg=msg, border='#' * len(msg)))
-        except Exception as e:
-            self.log(f'ERROR: Could not fetch update: {e}')
-
-    def stop(self):
-        self.auto_update.stop(False)
-
-    disable = shutdown_notification = stop
 
 
 class PeriodicJob(Thread):
@@ -109,3 +62,120 @@ class PeriodicJob(Thread):
         self.__pause.set()
 
 
+class BasePlugin(NBasePlugin):
+    settings = metasettings = {}
+    default_settings = {
+        'check_update': True,
+    }
+    default_metasettings = {
+        'check_update': {
+            'description': '''Check for Updates
+Check for updates on start and periodically''',
+            'type': 'bool',
+        },
+    }
+
+    __name__ = CONFIG.get('Name')
+    update_version = None
+
+    def init(self):
+        settings = self.default_settings
+        settings.update(self.settings)
+        self.settings = settings
+        metasettings = self.default_metasettings
+        metasettings.update(self.metasettings)
+        self.metasettings = metasettings
+
+        self.auto_update = PeriodicJob(name='AutoUpdate',
+                                       delay=3600,
+                                       update=self.check_update)
+        self.auto_update.start()
+
+        self.settings_watcher = PeriodicJob(name='SettingsWatcher', update=self.detect_settings_change)
+        self.settings_watcher.start()
+
+        self.log(f'Running version {__version__}')
+
+    def check_update(self):
+        try:
+            repo = CONFIG.get('Repository')
+            if 'dev' in __version__ or not repo or not self.settings['check_update']:
+                self.update_version = None
+                return
+
+            with request.urlopen(f'https://api.github.com/repos/{repo}/tags') as response:
+                latest_info = next(iter(json.load(response)), {})
+                latest_version = latest_info.get('name', '')
+                if latest_version.replace('v', '') != __version__:
+                    self.update_version = latest_version
+                    msg = f'# A new version of "{self.__name__}" is available: {latest_version} https://github.com/{repo}/releases/tag/{latest_version}'  # noqa
+                    self.log('\n{border}\n{msg}\n{border}'.format(msg=msg, border='#' * len(msg)))
+        except Exception as e:
+            self.log(f'ERROR: Could not fetch update: {e}')
+
+    def stop(self):
+        self.auto_update.stop(False)
+
+    disable = shutdown_notification = stop
+
+    def detect_settings_change(self):
+        if not hasattr(self, '_settings_before'):
+            self._settings_before = set(self.settings.items())
+            return
+
+        after = set(self.settings.items())
+        if changes := self._settings_before ^ after:
+            change_dict = {
+                'before': dict(filter(lambda i: i in self._settings_before, changes)),
+                'after': dict(filter(lambda i: i in after, changes))
+            }
+            self.settings_changed(before=self._settings_before,
+                                  after=self.settings,
+                                  change=change_dict)
+            self._settings_before = set(self.settings.items())
+
+    def settings_changed(self, before, after, change):
+        self.log(f'Settings change: {json.dumps(change)}')
+
+
+class Plugin(BasePlugin):
+    settings = {
+        'public_replies': 'i/test=Test failed',
+        'private_replies': '',
+    }
+    metasettings = {
+        'public_replies': {
+            'description': '''Public Replies
+Each line represents "incoming message=reply". Only applies to public chat rooms.
+You can use these placeholders: Senders name: {sender}, Own name: {self}, Room name: {room}
+Start the message with "i/" to make it case insensitive.
+Commands also work.
+
+Example:
+i/test=test failed
+hey=Hello {user}
+uwu=UMU''',
+            'type': 'textview',
+        },
+        'private_replies': {
+            'description': '''Private Replies
+Works the same as public replies but for private chats only.''',
+            'type': 'textview',
+        },
+    }
+
+    def init(self):
+        super().init()
+        self.parse_settings()
+
+    def settings_changed(self, before, after, change):
+        super().settings_changed(before, after, change)
+        if 'replies' in after:
+            self.parse_settings()
+
+    def parse_settings(self):
+        self.private_replies = self.public_replies = {}
+        for incoming, outgoing in map(lambda l: l.split('=', 1), filter(None, map(str.strip, self.settings['public_replies'].split('\n')))):  # noqa
+            self.public_replies[incoming] = outgoing
+        for incoming, outgoing in map(lambda l: l.split('=', 1), filter(None, map(str.strip, self.settings['private_replies'].split('\n')))):  # noqa
+            self.private_replies[incoming] = outgoing
